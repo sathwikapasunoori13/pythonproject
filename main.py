8d6b60d3b68afe364b396b7e3cf069a76703a9a0
sathwika <pasunoorisathwika1310@gmail.com>
2024-07-24 14:40:13 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import phonenumbers
+from test import  number
+
+from phonenumbers import geocoder
+ch_num=phonenumbers.parse(number,"TS")
+print(geocoder.description_for_number(ch_num,"en"))
+
+from phonenumbers import carrier
+service_num=phonenumbers.parse(number,"RO")
+print(carrier.name_for_number(service_num,"en"))
